ff17f27a5306cb940efc693ebdd35dd23c792fcd
Iftikhar uddin <iftikhar@connextar.com>
2024-01-26 14:54:54 +0500

    Rust data types and functions

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -2,4 +2,12 @@ fn main() {
     let var1 = 10;
     let var2 = 19;
     println!("Hello, world! Back to rust again ;) {} {}", var1, var2);
+    another();
+}
+
+fn another() {
+    let unsigned: u8 = 2;
+
+    let signed: i8 = -10;
+    println!("{} {}", signed, unsigned);
 }
